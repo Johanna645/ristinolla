b3e3f8fae5a3d4ef1e671ea07c410e6017deaacf
Johanna Räihälä <johanna.raihala@gmail.com>
2021-05-28 10:22:44 +0200

    remove eslint to allow index as a key

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { useGameState } from './GameState';
+
+type Value = 'X' | 'O' | null;
+type BoardState = Value[];
+type SquareProps = {
+  value: Value;
+  onClick: () => void;
+};
+type BoardProps = {
+  board: BoardState;
+  onClick: (square: number) => void;
+};
+type LogProps = {
+  history: BoardState[];
+  jumpTo: (step: number) => void;
+};
 
 function Game() {
+  const { gameState, current, xIsNext, jumpTo, winner, handleClick } =
+    useGameState();
+
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col">
-            <div>status</div>
+            <div>
+              {winner
+                ? `${winner} has won the game!`
+                : `Next player: ${xIsNext ? 'X' : 'O'}`}
+            </div>
 
-            <Board />
+            <Board board={current} onClick={handleClick} />
           </div>
 
           <div className="col">
-            <Log />
+            <Log history={gameState.history} jumpTo={jumpTo} />
           </div>
         </div>
       </div>
@@ -20,29 +43,36 @@ function Game() {
   );
 }
 
-function Board() {
+function Board({ board, onClick }: BoardProps) {
+  // create properties for a square in the given index
+  const createProps = (square: number): SquareProps => {
+    return {
+      value: board[square],
+      onClick: () => onClick(square),
+    };
+  };
   return (
     <div>
       <div className="container">
-        <div className="row gx-6 gy-6">
+        <div className="row">
           <div className="col">
-            <Square />
-            <Square />
-            <Square />
+            <Square {...createProps(0)} />
+            <Square {...createProps(1)} />
+            <Square {...createProps(2)} />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <Square />
-            <Square />
-            <Square />
+            <Square {...createProps(3)} />
+            <Square {...createProps(4)} />
+            <Square {...createProps(5)} />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <Square />
-            <Square />
-            <Square />
+            <Square {...createProps(6)} />
+            <Square {...createProps(7)} />
+            <Square {...createProps(8)} />
           </div>
         </div>
       </div>
@@ -50,23 +80,37 @@ function Board() {
   );
 }
 
-function Square() {
+function Square(props: SquareProps) {
   return (
-    <button type="button" className="btn btn-outline-dark btn-lg">
-      X
+    <button
+      type="button"
+      className="btn btn-outline-dark btn-lg"
+      onClick={props.onClick}
+    >
+      {props.value}
     </button>
   );
 }
 
-function Log() {
+function Log(props: LogProps) {
   return (
     <div>
       <ol>
-        <li>
-          <button type="button" className="btn btn-outline-dark btn-lg">
-            Go to move
-          </button>
-        </li>
+        const moves ={' '}
+        {props.history.map((step, move) => {
+          const desc = move ? 'Go to move #' + move : 'Go to game start';
+          return (
+            <li key={move}>
+              <button
+                type="button"
+                className="btn btn-outline-dark btn-lg"
+                onClick={() => props.jumpTo(move)}
+              >
+                {desc}
+              </button>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
